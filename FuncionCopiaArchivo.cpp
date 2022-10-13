35b056ed511dc0767394bf19665d54fb065ea23e
Willian Coral <coralw2210@gmail.com>
2022-10-13 01:59:46 -0400

    Se ha quitado el error del nombre de la copia si tenia un 2 antes del '.' y se ha añadido un mensaje cuando no encuentra el archivo

--- FuncionCopiaArchivo.cpp
+++ FuncionCopiaArchivo.cpp
@@ -10,42 +10,56 @@ void copia(const char*);
 int main(){
     system("cls");
     
-    copia("nombre.txt");
+    copia("nombres.txt");
     return 0;
 }
 
 
 void copia(const char* nombre_archivo){
     FILE *arch1, *arch2;
-    int aux=0;
-    char nombre_copia[strlen(nombre_archivo)];
-
-
-    strcpy(nombre_copia, nombre_archivo);
-
-    for(int i=0; i<strlen(nombre_archivo); i++){
-        if(nombre_copia[i+1]=='.'){
-            nombre_copia[i]='2';
-        }
-
-    }
-
 
     arch1=fopen(nombre_archivo,"r");
-    arch2=fopen(nombre_copia,"w");
+    if(arch1!=NULL){
+        int aux=0;
+        char nombre_copia[strlen(nombre_archivo)];
 
-    if(arch1!=NULL && arch2!=NULL){
-        
-        while(!feof(arch1)){
-            fread(&aux, sizeof(char), 1, arch1);
-            if(!feof(arch1)){
-                fwrite(&aux, sizeof(char), 1, arch2);
+
+        strcpy(nombre_copia, nombre_archivo);
+
+        for(int i=0; i<strlen(nombre_archivo); i++){
+            if(nombre_copia[i+1]=='.'){
+                if(nombre_copia[i]!='2'){
+                    nombre_copia[i]='2';
+                }
+                else{
+                    nombre_copia[i]='1';
+                }
             }
-            
 
         }
-        fclose(arch1);
-        fclose(arch2);
+
+
+    
+        arch2=fopen(nombre_copia,"w");
+
+        if(arch1!=NULL && arch2!=NULL){
+        
+            while(!feof(arch1)){
+                fread(&aux, sizeof(char), 1, arch1);
+                if(!feof(arch1)){
+                    fwrite(&aux, sizeof(char), 1, arch2);
+                }
+            
+
+            }
+            fclose(arch1);
+            fclose(arch2);
+        }
+
     }
+    else{
+        cout<<"\n\nNO SE HA ENCONTRADO ESE ARCHIVO\n\n";
+    }
+    
 
 }
