687aa1375286cc310f8d03acc7de21958bc70339
Willian Coral <coralw2210@gmail.com>
2022-10-13 02:39:54 -0400

    cambiada la sintaxis a copia(nombreArchivo,nombreCopiaArchivo);

--- FuncionCopiaArchivo.cpp
+++ FuncionCopiaArchivo.cpp
@@ -5,40 +5,22 @@
 
 using namespace std;
 
-void copia(const char*);
+void copia(const char*,const char*);
 
 int main(){
     system("cls");
     
-    copia("nombres.txt");
+    copia("nombre.txt", "nombrecopia.txt");
     return 0;
 }
 
 
-void copia(const char* nombre_archivo){
+void copia(const char* nombre_archivo,const char* nombre_copia){
     FILE *arch1, *arch2;
 
     arch1=fopen(nombre_archivo,"r");
     if(arch1!=NULL){
         int aux=0;
-        char nombre_copia[strlen(nombre_archivo)];
-
-
-        strcpy(nombre_copia, nombre_archivo);
-
-        for(int i=0; i<strlen(nombre_archivo); i++){
-            if(nombre_copia[i+1]=='.'){
-                if(nombre_copia[i]!='2'){
-                    nombre_copia[i]='2';
-                }
-                else{
-                    nombre_copia[i]='1';
-                }
-            }
-
-        }
-
-
     
         arch2=fopen(nombre_copia,"w");
 
